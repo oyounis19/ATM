316d513a022eef35d8fc937f6c7fa2e103c51bdd
AmrKhaled <amrkalledsaleh1@gmail.com>
2023-05-01 22:52:16 +0300

    admin class added

---
+++ Models/admin.php
@@ -0,0 +1,22 @@
+<?php
+    require_once "User.php";
+    class admin extends User{
+        private string $userName;
+        private string $passWord;
+
+        public function login(){}
+        public function logout(){}
+        public function addCustomer(){}
+        public function editCustomer(){}
+        public function deleteCustomer(){}
+        public function createAccount(){}
+        public function deleteAccount(){}
+        public function unblockCreditCard(){}
+        public function viewCustomers(){}
+        public function viewAtmTransactions(){}
+        public function addAtm(){}
+        public function deleteAtm(){}
+        public function editAtm(){}
+        public function viewAtm(){}
+
+    }
