2c6050b0ff9a24cb80b7462f31b9c6b2129f9f6c
unknown <cachamoudao9@gmail.com>
2023-05-01 20:34:13 +0300

    5elset ya sya3

---
+++ Models/Transaction.php
@@ -0,0 +1,62 @@
+<?php
+    class Transaction
+    {
+        private string $type;
+        private string $time;
+        private $date;
+        private $amount;
+        private DBconnector $e;
+
+    
+	public function getType() {
+		return $this->type;
+	}
+	
+	
+	public function setType(string $type){
+		$this->type = $type;
+		return $this;
+	}
+    
+
+
+	public function getTime(): string {
+		return $this->time;
+	}
+	
+	
+	public function setTime(string $time): self {
+		$this->time = $time;
+		return $this;
+	}
+
+	
+	public function getDate() {
+		return $this->date;
+	}
+	
+	
+	public function setDate($date): self {
+		$this->date = $date;
+		return $this;
+	}
+
+	
+	public function getAmount() {
+		return $this->amount;
+	}
+	
+	public function setAmount($amount): self {
+		$this->amount = $amount;
+		return $this;
+	}
+    public function saveTransaction($accountid , $SSN, ATM $x ) 
+    {
+        $e=new DBconnector();
+        $e ->dbconnect();
+        $e ->modify("insert into `Transaction`(Account_ID , SSN, ATM_ID, Amount ,`Date` , State , Type , recipient_account_ID ) 
+        values($accountid , $SSN , ".$x->getID()." ,".$this ->amount." , ".$this->date." ,  )");
+
+    } 
+}
+?>
