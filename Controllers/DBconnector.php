51d48f920cd7276964d6b0fbb0f68b727e3df1a5
AhmedEbrahim2322004 <ahmed2322004ahmed@gmail.com>
2023-05-04 12:26:03 +0200

    the simple version of DBconnector

--- Controllers/DBconnector.php
+++ Controllers/DBconnector.php
@@ -18,121 +18,40 @@ class DBConnector {
         }
     }
 
-    /**
-     * Retrieves rows from the specified table and returns them as an array of associative arrays.
-     *
-     * @param string $table The name of the table to select from.
-     * @param string|array $columns The column(s) to select.
-     * @param string|null $where The WHERE clause of the query, without the "WHERE" keyword.
-     * @return array An array of associative arrays representing the rows selected.
-     * @throws Exception If there was an error executing the select query.
-     */
-    public function select($table, $columns = "*", $where = null) {
-        $query = "SELECT $columns FROM $table";
-        if ($where) {
-            $query .= " WHERE $where";
+    # this function for Database SELECT queries
+    // input: Mysql query
+    public function select($sqlqry){
+        try{
+            $result = $this->connection->query($sqlqry);
+            // query function return false if the query is false and sql object if true
+            if($result == false)
+                throw new Exception();
+
+            return $result->fetch_assoc();
         }
-        $stmt = $this->connection->prepare($query);
-        if (!$stmt) {
-            throw new Exception("Failed to prepare query: " . $this->connection->error);
+        catch(Exception $e){
+            //use the below statment to know the error
+            echo "message: " . $e->getMessage();
+            return false;
         }
-        if (!$stmt->execute()) {
-            throw new Exception("Failed to execute query: " . $stmt->error);
-        }
-        $result = $stmt->get_result();
-        return $result->fetch_assoc();
     }
 
-    /**
-     * Inserts a new record into the specified table with the given data.
-     *
-     * @param string $table The name of the table to insert into.
-     * @param array $data An array of values to insert into the table.
-     * @return int The number of rows affected by the insert operation.
-     * @throws Exception If there was an error executing the insert query.
-     */
-    public function insert($table, $data) {
-        $columns = implode(",", array_keys($data));
-        $values = implode(",", array_fill(0, count($data), "?"));
-        $query = "INSERT INTO $table ($columns) VALUES ($values)";
-        $stmt = $this->connection->prepare($query);
-        if (!$stmt) {
-            throw new Exception("Failed to prepare query: " . $this->connection->error);
+    # this function for Database INSERT,DELETE,UPDATE queries
+    // input: Mysql query
+    public function modify($sqlqry){
+        // if there is no exeption the function return true else false
+        try{
+            $result = $this->connection->query($sqlqry);
+            if($result == false)
+                throw new Exception();
+            
+            return true;
         }
-        
-        $values = array_values($data);
-        $params = array(str_repeat("s", count($data)));
-        foreach ($values as $value) {
-            $params[] = $value;
+        catch(Exception $e){
+            //use the below statment to know the error
+            echo "message: " . $e->getMessage();
+            return false;
         }
-        
-        if (!$stmt->bind_param(...$params)) {
-            throw new Exception("Failed to bind parameters: " . $stmt->error);
-        }
-        
-        if (!$stmt->execute()) {
-            throw new Exception("Failed to execute query: " . $stmt->error);
-        }
-        return $stmt->affected_rows;
-    }
-
-    /**
-     * Update a record with the specified table with the given data.
-     *
-     * @param string $table The name of the table to insert into.
-     * @param array $data An array of values to insert into the table.
-     * @param string $where The WHERE clause of the query, without the "WHERE" keyword.
-     * @return int The number of rows affected by the insert operation.
-     * @throws Exception If there was an error executing the insert query.
-     */    
-    public function update($table, $data ,$where) {
-        $query = "UPDATE $table SET ";
-        foreach($data as $key => $value){
-            $query .= "$key = ? ,";
-        }
-        $query = rtrim($query,",");
-        $query .= "WHERE $where";
-        $stmt = $this->connection->prepare($query);
-        if (!$stmt) {
-            throw new Exception("Failed to prepare query: " . $this->connection->error);
-        }
-        
-        $values = array_values($data);
-        $params = array(str_repeat("s", count($data)));
-        foreach ($values as $value) {
-            $params[] = $value;
-        }
-        
-        if (!$stmt->bind_param(...$params)) {
-            throw new Exception("Failed to bind parameters: " . $stmt->error);
-        }
-        
-        if (!$stmt->execute()) {
-            throw new Exception("Failed to execute query: " . $stmt->error);
-        }
-        return $stmt->affected_rows;
-    }
-
-
-    /**
-     * Deletes rows from the specified table that match the given condition.
-     *
-     * @param string $table The name of the table to delete from.
-     * @param string|null $where The WHERE clause of the query, without the "WHERE" keyword.
-     * @param array $params An array of values to bind to the query.
-     * @return int The number of rows affected by the delete operation.
-     * @throws Exception If there was an error executing the delete query.
-     */
-    public function delete($table, $where) {
-        $query = "DELETE FROM $table WHERE $where";
-        $stmt = $this->connection->prepare($query);
-        if (!$stmt) {
-            throw new Exception("Failed to prepare query: " . $this->connection->error);
-        } 
-        if (!$stmt->execute()) {
-            throw new Exception("Failed to execute query: " . $stmt->error);
-        }
-        return $stmt->affected_rows;
     }
 
     /**
