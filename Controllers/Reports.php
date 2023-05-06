6cc5fcc204c9367d2542ab2d4f774af5b28f7404
unknown <cachamoudao9@gmail.com>
2023-05-06 23:55:06 +0300

    done

--- Controllers/Reports.php
+++ Controllers/Reports.php
@@ -49,7 +49,53 @@ class Report{
     }
 
     public function generateTechPDF(){
+        //remove deafault header and footer
+        $this->pdf->setPrintFooter(false);
+        $this->pdf->setPrintHeader(false);
+
+        //Add 1 page
+        $this->pdf->AddPage();
+        // $this->pdf->Cell(190, 10, "this is a cell", 1, 1, 'C');
         
+        $this->pdf->writeHTMLCell(190,1, '', '', '<h1 style="text-align: center;">Technican Report</h1>',1, 1);
+        $this->pdf->writeHTMLCell(21.11,15, '', '', '<h4 style="text-align: center;">Trans<br>ID</h4>',1, 0);
+        $this->pdf->writeHTMLCell(21.11,15, '', '', '<h4 style="text-align: center;">Account<br>ID</h4>',1, 0);
+        $this->pdf->writeHTMLCell(21.11,15, '', '', '<h4 style="text-align: center;">SSN</h4>',1, 0);
+        $this->pdf->writeHTMLCell(21.11,15, '', '', '<h4 style="text-align: center;">ATM<br>ID</h4>',1, 0);
+        $this->pdf->writeHTMLCell(21.11,15, '', '', '<h4 style="text-align: center;">Amount</h4>',1, 0);
+        $this->pdf->writeHTMLCell(21.11,15, '', '', '<h4 style="text-align: center;">Date</h4>',1, 0);
+        $this->pdf->writeHTMLCell(21.11,15, '', '', '<h4 style="text-align: center;">State</h4>',1, 0);
+        $this->pdf->writeHTMLCell(21.11,15, '', '', '<h4 style="text-align: center;">Type</h4>',1, 0);
+        $this->pdf->writeHTMLCell(21.11,15, '', '', '<h4 style="text-align: center;">Reciver<br>Id</h4>',1, 1);
+        //Getting data from Database
+        $result = $this->db->select("`Transaction`","*");
+        $columnsNames = array("ID","AccountID", "SSN", "AtmID", "Amount", "Date", "State", "Type" , "receiverId" );
+
+        if(!$result){
+            return false;
+        }else
+        {
+            for($i=0; $i<sizeof($result); $i++){
+                
+                 for($j=0; $j<sizeof($result[$i])-1; $j++){
+                    if([$i]["State"]== "Approved"){
+                        $this->pdf->writeHTMLCell(21.11,15, '', '', '<h4 style="text-align: center; color:green">Approved</h4>',1, 1);
+                    }
+                    else{
+                        $this->pdf->writeHTMLCell(21.11,15, '', '', '<h4 style="text-align: center; color:green">De</h4>',1, 1);
+                    } 
+                    $this->pdf->writeHTMLCell(21.11,15, '', '', '<h4 style="text-align: center;">'.$result[$i][$columnsNames[$j]].'</h4>',1, 0);
+                    
+                     }
+                }                
+                $this->pdf->writeHTMLCell(21.11,15, '', '', '<h4 style="text-align: center;">'.$result[$i]["receiverId"].'</h4>',1, 1);
+
+            }
+        
+        //Add content
+        
+        $this->pdf->Output();
     }
+    
 }
 ?>
