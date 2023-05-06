a23d4687c66c73da5c05cae14299db8618f3037f
omar younis <oyounis19@gmail.com>
2023-05-06 17:01:14 +0300

    Add pdf reports library

---
+++ Models/Reports.php
@@ -0,0 +1,21 @@
+<?php
+require_once '../Controllers/lib/PHPMailer/src/PHPMailer.php';
+include('../Controllers/lib/TCPDF/tcpdf.php');
+
+class Report{
+
+    private $pdf = new TCPDF('P', 'mm', 'A4');
+    public function generateAdminPDF(){
+        
+        $this->pdf->setPrintFooter(false);
+        $this->pdf->setPrintHeader(false);
+        //Add 1 page
+        $this->pdf->AddPage();
+
+        //Add content
+    }
+}
+
+
+
+?>
