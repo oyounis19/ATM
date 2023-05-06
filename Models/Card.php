98a27ca7cb5830aaaf47bbd91dd9681d0a9a91f9
AhmedEbrahim2322004 <ahmed2322004ahmed@gmail.com>
2023-05-06 14:21:57 +0200

    Create Customer done

--- Models/Card.php
+++ Models/Card.php
@@ -53,5 +53,39 @@ class card{
 		$this->id = $id;
 		return $this;
 	}
+
+	public function generateCardID(){
+		$card_number = rand(1,9);
+		for ($i = 1; $i < 16; $i++) {
+			$card_number .= rand(0, 9);
+		}
+		return $card_number;
+	}
+
+	function generateCVV() {
+		$CVV_number = rand(1,9);
+		for ($i = 1; $i < 3; $i++) {
+			$CVV_number .= rand(0, 9);
+		}
+		return $CVV_number;
+	}
+	
+	function generateExpDate(){
+		// Get today's date
+		$today = new DateTime();
+
+		// Add 3 years to today's date
+		$three_years_from_today = $today->add(new DateInterval('P3Y'));
+
+		// Print the result
+		return $three_years_from_today->format('Y-m-d');
+	}
+
+	function generateCard(){
+		$this->setId($this->generateCardID());
+        $this->setCVV($this->generateCVV());
+        $this->setDate($this->generateExpDate());
+		return $this;
+	}
 }
 ?>
--- View/admin/CreateUser.php
+++ View/admin/CreateUser.php
@@ -2,10 +2,21 @@
 require_once (__DIR__."/Head.php");
 require_once(__DIR__."/../../Models/customer.php");
 
-if(isset($_POST["fnmae"])&&isset($_POST["lname"])&&isset($_POST["email"])&&isset($_POST["street"])&&isset($_POST["area"])
-&&isset($_POST["city"])&&isset($_POST["SSN"])&&isset($_POST["fprint"])&&isset($_POST["PIN"])){
+$showAlert = 0;
+echo "ahmed";
+
+$hashedFingerPrint = false;
+
+if(isset($_FILES['fprint'])){
+    $target_file1 = $_FILES['fprint']["tmp_name"];
+    $hashedFingerPrint = md5_file ($target_file1);
+    echo $hashedFingerPrint."<br>";
+}
+
+if(isset($_POST["fname"]) && isset($_POST["lname"]) && isset($_POST["email"]) && isset($_POST["street"]) && isset($_POST["area"])
+&& isset($_POST["city"]) && isset($_POST["SSN"]) &&  isset($_POST["PIN"]) && $hashedFingerPrint){
     $customer = new customer();
-    $customer->setFirstName($_POST["fnmae"]);
+    $customer->setFirstName($_POST["fname"]);
     $customer->setLastName($_POST["lname"]);
     $customer->setEmail($_POST["email"]);
     $customer->setStreet($_POST["street"]);
@@ -13,14 +24,17 @@ if(isset($_POST["fnmae"])&&isset($_POST["lname"])&&isset($_POST["email"])&&isset
     $customer->setCity($_POST["city"]);
     $customer->setSSN($_POST["SSN"]);
     $customer->setPin($_POST["PIN"]);
-
-    $target_file = $_FILES['fprint']["tmp_name"];
-    $hashedFingerPrint = md5_file ($target_file);
-    // echo $hash."<br>";
     $customer->setFingerprint($hashedFingerPrint);
 
     $admin = new Admin("","");
+    echo "ahmed";
     $result = $admin->addCustomer($customer);
+    echo "ahmed2";
+    if ($result) 
+        $showAlert = 1;
+    else 
+        $showAlert = 2;
+    
 }
 ?>
 
@@ -35,7 +49,7 @@ if(isset($_POST["fnmae"])&&isset($_POST["lname"])&&isset($_POST["email"])&&isset
        <section class="createUser screen" id="createUser">
         <div class="container-fluid">
             <h2>Create User</h2>
-            <form action="#" method="post">
+            <form action="#" method="post" enctype="multipart/form-data">
                 <div class="name d-flex gap-4">
 
                     <div class="form-floating w-50 mb-3">
@@ -94,3 +108,49 @@ if(isset($_POST["fnmae"])&&isset($_POST["lname"])&&isset($_POST["email"])&&isset
     <!-- end createUser  -->
     </div>
 </body>
+
+<?php
+    if ($showAlert == 1) {
+
+    ?>
+        <script>
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+            })
+
+            Toast.fire({
+                icon: 'success',
+                title: 'New customer created successfully'
+            })
+        </script>
+    <?php
+    } else if ($showAlert == 2) {
+    ?>
+        <script>
+            const Tooast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+            })
+
+            Tooast.fire({
+                icon: 'error',
+                title: 'something went wrong with creating the customer'
+            })
+        </script>
+    <?php
+    } ?>
