a586a0494f15112b8d4e5756d98ca1134c849fb2
AmrKhaled <amrkalledsaleh1@gmail.com>
2023-05-06 10:34:08 +0300

    hi

--- View/admin/home.php
+++ View/admin/home.php
@@ -1,7 +1,9 @@
 <?php
 require_once __DIR__ . "/../../Models/admin.php";
 
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 $userID = "";
 $firstname = "";
 $lastname = "";
