623785fe5b45c3d5f03304c69e39d21b2b3bf8ce
omar younis <oyounis19@gmail.com>
2023-05-07 11:37:05 +0300

    gmail

--- Models/ATM.php
+++ Models/ATM.php
@@ -50,7 +50,7 @@ class ATM{
             $mail->Host       = 'smtp.gmail.com';                       //Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
             $mail->Username   = 'atm86596@gmail.com';                //SMTP username
-            $mail->Password   = 'yyafjkepoqlrcxye';                     //SMTP password
+            $mail->Password   = 'qlualdikbwjtwfir';                     //SMTP password
             $mail->SMTPSecure = 'tls';                                  //Enable implicit TLS encryption
             $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
             $mail->isHTML(true);   
