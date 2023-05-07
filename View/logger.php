cc9dd285f79dc522347884f947f18eda3f12c91e
omar younis <oyounis19@gmail.com>
2023-05-07 20:04:55 +0300

    sss

--- View/logger.php
+++ View/logger.php
@@ -33,11 +33,11 @@ $result =  $srvTeq->checkLoggers();
         <div class="screens d-flex">
             <div class="screen history">
                 <h2 class="text-white fw-bolder">Logger</h2>
-                 <?php 
-                 for( $i = 0 ; $i < sizeof($result);$i++){
+                <div class="transactions">
+                <?php 
+                for($i = 0 ; $i < sizeof($result);$i++){
                     ?>
-                <div class="transictions">
-                    <div id="transiction" class="transiction d-flex mt-4 justify-content-between">
+                    <div class="transaction d-flex mt-4 justify-content-between" id="<?php echo $i+1;?>">
                         <div class="accountInfo">
                             <ul>
                                 <li>
@@ -50,16 +50,12 @@ $result =  $srvTeq->checkLoggers();
                                     <p>Date : <span><?php echo $result[$i]["Date"] ?></span></p>
                                 </li>
                             </ul>
-
                         </div>
-
                         <i class="fa-solid fa-arrow-up"></i>
-
-
                     </div>
-                 <?php 
-                 }
-                 ?>
+                <?php 
+                }
+                ?>
                 </div>
             </div>
                 
@@ -67,87 +63,39 @@ $result =  $srvTeq->checkLoggers();
     </div>
 
     <div class="popup-cards">
-        <div class="popup flex-column" pop="true" id="1-popup">
-            <i id="close" class="fa-solid fa-xmark"></i>
-            <h2 class="text-white fs-1 mb-5">Logger Details</h2>
-            <ul class="text-start text-white">
-                <li>
-                    <p>Account ID : <span>555555555555555</span></p>
-                </li>
-                <li>
-                    <p>Transaction Type : <span>Withdraw</span></p>
-                </li>
-                <li>
-                    <p>Transaction ID : <span>20230321</span></p>
-                </li>
-                <li>
-                    <p>State : <span>2023/3/1</span></p>
-                </li>
-                <li>
-                    <p>Amount : <span>200 LE</span></p>
-                </li>
-                <li>
-                    <p>Date : <span>2023/3/1</span></p>
-                </li>
-                <li>
-                    <p>time : <span>20:08</span></p>
-                </li>
-            </ul>
-        </div>
-        <div class="popup flex-column" pop="true" id="2-popup">
-            <i id="close" class="fa-solid fa-xmark"></i>
-            <h2 class="text-white fs-1 mb-5">Logger Details</h2>
-            <ul class="text-start text-white">
-                <li>
-                    <p>Account ID : <span>4444444444444</span></p>
-                </li>
-                <li>
-                    <p>Transaction Type : <span>Withdraw</span></p>
-                </li>
-                <li>
-                    <p>Transaction ID : <span>20230321</span></p>
-                </li>
-                <li>
-                    <p>State : <span>2023/3/1</span></p>
-                </li>
-                <li>
-                    <p>Amount : <span>200 LE</span></p>
-                </li>
-                <li>
-                    <p>Date : <span>2023/3/1</span></p>
-                </li>
-                <li>
-                    <p>time : <span>20:08</span></p>
-                </li>
-            </ul>
-        </div>
-        <div class="popup flex-column" pop="true" id="3-popup">
-            <i id="close" class="fa-solid fa-xmark"></i>
-            <h2 class="text-white fs-1 mb-5">Logger Details</h2>
-            <ul class="text-start text-white">
-                <li>
-                    <p>Account ID : <span>111111111</span></p>
-                </li>
-                <li>
-                    <p>Transaction Type : <span>Withdraw</span></p>
-                </li>
-                <li>
-                    <p>Transaction ID : <span>20230321</span></p>
-                </li>
-                <li>
-                    <p>State : <span>2023/3/1</span></p>
-                </li>
-                <li>
-                    <p>Amount : <span>200 LE</span></p>
-                </li>
-                <li>
-                    <p>Date : <span>2023/3/1</span></p>
-                </li>
-                <li>
-                    <p>time : <span>20:08</span></p>
-                </li>
-            </ul>
-        </div>
+        <?php
+        for( $i = 0 ; $i < sizeof($result);$i++){
+        ?>
+            <div class="popup flex-column" pop="true" id="<?php echo $i+1;?>-popup">
+                <i id="close" class="fa-solid fa-xmark"></i>
+                <h2 class="text-white fs-1 mb-5">Logger Details</h2>
+                <ul class="text-start text-white">
+                    <li>
+                        <p>Account ID : <span>555555555555555</span></p>
+                    </li>
+                    <li>
+                        <p>Transaction Type : <span>Withdraw</span></p>
+                    </li>
+                    <li>
+                        <p>Transaction ID : <span>20230321</span></p>
+                    </li>
+                    <li>
+                    <p>State : <span value="<?php echo $state ?>"><?php echo $state ?></span></p>
+                    </li>
+                    <li>
+                        <p>Amount : <span>200 LE</span></p>
+                    </li>
+                    <li>
+                        <p>Date : <span>2023/3/1</span></p>
+                    </li>
+                    <li>
+                        <p>time : <span>20:08</span></p>
+                    </li>
+                </ul>
+            </div>
+        <?php 
+        }
+        ?>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
